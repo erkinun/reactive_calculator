35a263d88d05ef85dcc6f000a2b7afe7dcf8aa36
lost_mohican <erkinun@gmail.com>
2015-04-23 12:00:44 +0300

    polynomial solver is implemented, returns empty set when delta is 0

--- src/main/scala/calculator/Polynomial.scala
+++ src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,33 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Signal {
+      val bVal = b()
+      val aVal = a()
+      val cVal = c()
+
+      (bVal * bVal) - (4 * aVal * cVal)
+    }
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    Signal {
+
+      val bVal = b()
+      val aVal = a()
+
+      delta() match {
+        case d if d < 0 => Set()
+        case d if d == 0 => Set((-bVal) / (2 * aVal))
+        case d => {
+
+          Set(
+            (-bVal + math.sqrt(d)) / (2 * aVal),
+            (-bVal - math.sqrt(d)) / (2 * aVal)
+          )
+        }
+      }
+    }
   }
 }
