4b80ca4b35f4d2ed53a0f6d5bee2066e0582e701
lost_mohican <erkinun@gmail.com>
2015-04-23 11:30:15 +0300

    color signal is implemented

--- src/main/scala/calculator/TweetLength.scala
+++ src/main/scala/calculator/TweetLength.scala
@@ -8,7 +8,13 @@ object TweetLength {
   }
 
   def colorForRemainingCharsCount(remainingCharsCount: Signal[Int]): Signal[String] = {
-    ???
+    Signal {
+      remainingCharsCount() match {
+        case x if x >= 15 => "green"
+        case x if x <= 14 && x >= 0 => "orange"
+        case _ => "red"
+      }
+    }
   }
 
   /** Computes the length of a tweet, given its text string.
